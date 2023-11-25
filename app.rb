8615e836cd435dd981d889b565e95859ccff0496
David Le <43181047+DavidVLe1@users.noreply.github.com>
2023-11-25 08:09:26 +0000

    completed routes for sq,sq_rt, random and results

--- app.rb
+++ app.rb
@@ -2,8 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @the_num= params.fetch("number")
+  @the_result= @the_num.to_f ** 2
+  erb(:square_results)
+end
+
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+get("/square_root/results") do
+  @the_num=params.fetch("number")
+  @the_result= @the_num.to_f ** 0.5
+  erb(:square_root_results)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  @number1=params.fetch("number1").to_i
+  @number2=params.fetch("number2").to_i
+  @chosen_number=rand(@number1..@number2)
+  erb(:random_results)
+end
+
+get("/payment/new") do
+  erb(:payment)
 end
